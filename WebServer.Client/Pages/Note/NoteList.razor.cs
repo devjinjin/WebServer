feaecd54fe45a0b7f3d46b1387d7fa6bf219cad3
이진영 <devjinjin@gmail.com>
2021-03-23 19:48:35 +0900

    CRUD 작업중

---
+++ WebServer.Client/Pages/Note/NoteList.razor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebServer.Client.Pages.Note
+{
+    public partial class NoteList
+    {
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        public void MoveCreate()
+        {
+            //this.NavigationManager.NavigateTo("/note/create", forceLoad: true);
+            this.NavigationManager.NavigateTo("/note/create");
+
+        }
+
+        public void MoveUpdate()
+        {
+            //this.NavigationManager.NavigateTo("/note/create", forceLoad: true);
+            this.NavigationManager.NavigateTo("/note/update/"+1);
+
+        }
+
+        public void MoveDelete()
+        {
+            //this.NavigationManager.NavigateTo("/note/create", forceLoad: true);
+            this.NavigationManager.NavigateTo("/note/delete/" + 1);
+
+        }
+
+        public void MoveDetail()
+        {
+            //this.NavigationManager.NavigateTo("/note/create", forceLoad: true);
+            this.NavigationManager.NavigateTo("/note/detail/" + 1);
+
+        }
+    }
+}
